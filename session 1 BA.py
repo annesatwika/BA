192655c16338d1a35401928034f44490ca027a51
Anne Satwika Teli <um20252@stu.ximb.ac.in>
2020-09-05 22:21:33 +0530

    Create session 1 BA.py
    Session 1

---
+++ session 1 BA.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 20:13:26 2020
+
+@author: satwika
+"""
+A=10
+print(A)
+print?
+
+print("Anne Satwika Teli")
+print("Anne","Satwika","Teli",sep='==',end=".....")
+print("Anne","Satwika","Teli",sep='==')
+
+help(print)
+
+help("operators")
+
+!python --version
+!pip list
+
+a="anne"
+b=10
+c=30.33
+type(a)
+type(b)
+type(c)
+
+Num = int(Number)
+type(Num)
+
+PhoneNum=int(input())
+
+print("phone no is ",PhoneNum)
+
+country="India"
+print("I live in ",country," which is a very good country")
+print("I live in {0} which is a very good country".format(country))
+
+p=1.11
+q=int(a)
+r=float(q)
+
+X=0
+Y=1
+
+D= X and Y
+D
+E= X or Y
+E
+
+L=15
+bool(L)
+M=0
+bool(M)
+
+fname="anne"
+lname="teli"
+name= fname + " " + lname
+name
+
+len(lname)
+lname=lname.capitalize()
+print(lname)
+lname=lname.upper()
+print(lname)
+lname=lname.lower()
+print(lname)
+lname=lname.rjust(10)
+print(lname)
+lname=lname.center(20)
+print(lname)    
+name=name.replace('teli','satwika')
+print(name)
+
